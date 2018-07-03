978814783d16876abcae8489d8a674345df288b0
xuyanru <3131895796Xu>
2018-07-03 10:31:30 +0800

    filesetting

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,21 +5,49 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './test.component.html'
 })
 export class TestComponent implements OnInit {
+  //管道
   pi = 3.14;
   e = 2.718281828459045;
   date = new Date();
   date1 = '2018-05-07 11:04:33';
   date2 = new Date(parseInt('1530501260', 10) * 1000);  //10位数需要乘以 1000
   date3 = 1530501260;
-
+  //js 分组方法
+  list = [
+    {"name": "John", "Average": 15, "High": 10, "DtmStamp": 1358226000000},
+    {"name": "Jane", "Average": 16, "High": 92, "DtmStamp": 1358226000000},
+    {"name": "Jane", "Average": 17, "High": 45, "DtmStamp": 1358226000000},
+    {"name": "John", "Average": 18, "High": 87, "DtmStamp": 1358226000000},
+    {"name": "Jane", "Average": 15, "High": 10, "DtmStamp": 1358226060000},
+    {"name": "John", "Average": 16, "High": 87, "DtmStamp": 1358226060000},
+    {"name": "John", "Average": 17, "High": 45, "DtmStamp": 1358226060000},
+    {"name": "Jane", "Average": 18, "High": 92, "DtmStamp": 1358226060000}
+  ];
+  public groupByList;
   constructor() {
 
   }
 
   ngOnInit() {
+    this.groupByList = this.groupBy(this.list, function (item) {
+      return [item.name];
+    });
+    console.log(this.groupByList);
+  }
 
-    console.log(this.date2);
-
+  groupBy(array, f) {
+    console.log(f);
+    const groups = {};
+    array.forEach(function (o) {
+      const group = JSON.stringify(f(o));   //带入上面的方法得到name
+      groups[group] = groups[group] || [];  //没有push过是undefind  push过后将之前信息取出来
+      groups[group].push(o);
+    });
+    console.log(groups);   //此数组中 是 分组后的list 前面带有key
+    //Object.keys(groups)是取出groups对象中的所有key，然后遍历一个个key组成的新数组
+    return Object.keys(groups).map(function (group) {
+      return groups[group];
+    });
   }
 
 
