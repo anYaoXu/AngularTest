156e51366c0bc3b9ab908da4821633c089986025
xuyanru <3131895796Xu>
2019-01-03 18:29:19 +0800

    switch delete ngIf

--- src/app/common/share-controllers/common-switch/common-switch.component.ts
+++ src/app/common/share-controllers/common-switch/common-switch.component.ts
@@ -3,33 +3,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-common-switch',
   template: `
-    <span *ngIf="size">
-      <ui-switch
-        [checked]="checked"
-        [size]="size"
-        [disabled]="disabled"
-        [color]="bdColor"
-        [switchColor]="switchColor"
-        [uncheckedLabel]="uncheckedLabel"
-        [checkedLabel]="checkedLabel"
-        (valueChange)="onValueChange($event)"
-        (change)="onChange($event)"
-        (changeEvent)="onChangeEvent($event)"
-      ></ui-switch>
-    </span>
-    <span *ngIf="!size">
-      <ui-switch
-        [checked]="checked"
-        [disabled]="disabled"
-        [color]="bdColor"
-        [switchColor]="switchColor"
-        [uncheckedLabel]="uncheckedLabel"
-        [checkedLabel]="checkedLabel"
-        (valueChange)="onValueChange($event)"
-        (change)="onChange($event)"
-        (changeEvent)="onChangeEvent($event)"
-      ></ui-switch>
-    </span>
+    <ui-switch
+      [checked]="checked"
+      [size]="size"
+      [disabled]="disabled"
+      [color]="bdColor"
+      [switchColor]="switchColor"
+      [uncheckedLabel]="uncheckedLabel"
+      [checkedLabel]="checkedLabel"
+      (valueChange)="onValueChange($event)"
+      (change)="onChange($event)"
+      (changeEvent)="onChangeEvent($event)"
+    ></ui-switch>
   `,
   styles: [``]
   // styleUrls: ['./common-switch.component.css']
@@ -37,7 +22,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class CommonSwitchComponent implements OnInit {
   @Output() valueChangeEvent = new EventEmitter();
   @Input() public checked = true;
-  @Input() public size = null;
+  @Input() public size = 'medium';
   @Input() public disabled = false;
   @Input() public bdColor = '#0072c6';
   @Input() public switchColor = '#fff';
